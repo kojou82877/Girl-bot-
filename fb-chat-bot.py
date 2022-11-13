4d7739f76135878deb6082431a9c9d614b0317e3
kojou82877 <116699713+kojou82877@users.noreply.github.com>
2022-11-13 15:22:18 +0530

    Update fb-chat-bot.py

--- fb-chat-bot.py
+++ fb-chat-bot.py
@@ -1,5 +1,7 @@
 from fbchat import Client, log, _graphql
 from fbchat.models import *
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 import json
 import random
 import wolframalpha
@@ -12,7 +14,16 @@ import os
 import concurrent.futures
 from difflib import SequenceMatcher, get_close_matches
 
+chatbot = ChatBot('FreeBirdsBot')
 
+trainer = ListTrainer(chatbot)
+
+trainer.train(['Hi','Hello','How are you?','I am fine and You?','Greate','What are you Doing?','nothing just roaming around.'])
+
+while True:
+	input_data = input("You- ")
+	response = chatbot.get_response(input_data)
+	print("FreeBirdsBot- ",response)
 
 class ChatBot(Client):
 
